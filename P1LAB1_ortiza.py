ab3ae732556da4202b6fd247dc1e6b8b76f53554
Ortizaftcc <144837638+Ortizaftcc@users.noreply.github.com>
2023-12-05 13:17:23 -0500

    Add files via upload

---
+++ P1LAB1_ortiza.py
@@ -0,0 +1,20 @@
+"""
+CTI 110
+P1LAB1
+Ortiza
+8/24/2023
+"""
+
+# Say hello to the user
+print("Hello! What is your name?")
+
+# get he user's name
+name = input()
+
+print(name, "is a nice name")
+
+
+print("what is your favorite video game?")
+name = input()
+
+print(name, "Thats a pretty cool game")
